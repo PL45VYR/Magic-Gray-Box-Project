e8725eb022c0e9d7b9f97cdd229ad55f2a96bd0f
PL45VYR <224156751+PL45VYR@users.noreply.github.com>
2026-05-03 12:29:41 -0400

    Added engineering block recipes

--- kubejs/server_scripts/mods/immersive.js
+++ kubejs/server_scripts/mods/immersive.js
@@ -127,4 +127,89 @@ ServerEvents.recipes(event => {
             "id": "immersiveengineering:wirecoil_redstone"
         }
     })
+
+    // Engineering Block Recipes
+    // Light Engineering
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:light_engineering',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_iron',
+            "C": 'immersiveengineering:component_iron',
+            "I": 'create:precision_mechanism'
+        }
+    )
+
+    // Redstone Engineering
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:rs_engineering',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_iron',
+            "C": 'minecraft:redstone',
+            "I": 'create_connected:control_chip'
+        }
+    )
+
+    // Heavy Engineering
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:heavy_engineering',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_steel',
+            "C": 'immersiveengineering:component_steel',
+            "I": '#c:ingots/electrum'
+        }
+    )
+
+    // Radiator
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:radiator',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_steel',
+            "C": '#c:plates/constantan',
+            "I": 'minecraft:water_bucket'
+        }
+    )
+
+    // Generator
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:generator',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_steel',
+            "C": 'immersiveengineering:coil_mv',
+            "I": 'immersiveengineering:component_iron'
+        }
+    )
+
+    // Resonanz Engineering
+    event.recipes.extendedcrafting.shaped_table('immersiveengineering:resonanz_engineering',
+        [
+            "SCS",
+            "CIC",
+            "SCS"
+        ],
+        {
+            "S": 'immersiveengineering:sheetmetal_lead',
+            "C": 'immersiveengineering:component_electronic_adv',
+            "I": 'minecraft:echo_shard'
+        }
+    )
 })
