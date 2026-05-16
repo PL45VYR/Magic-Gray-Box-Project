121f169a8f147a7161b27708d31558e04113e844
PL45VYR <224156751+PL45VYR@users.noreply.github.com>
2026-05-16 09:37:00 -0400

    Added new Primogel recipe for T3 crafting
    Removed other goo recipes
    
    Created file for compact crafting recipes

--- kubejs/server_scripts/mods/justdire.js
+++ kubejs/server_scripts/mods/justdire.js
@@ -1,2 +1,31 @@
 ServerEvents.recipes(event => {
+    // Primogel Recipe
+    event.recipes.extendedcrafting.shaped_table('justdirethings:gooblock_tier1',
+        [
+            'CSACASC',
+            'SASASAS',
+            'ASCHCSA',
+            'CAHLHAC',
+            'ASCHCSA',
+            'SASASAS',
+            'CSACASC'
+        ],
+        {
+            'C': {
+                "item": 'minecraft:clay'
+            },
+            'S': {
+                "item": 'minecraft:sugar'
+            },
+            'A': {
+                "item": 'eidolon_repraised:enchanted_ash'
+            },
+            'H': {
+                "item": 'reliquary:zombie_heart'
+            },
+            'L': {
+                "item": 'malum:malignant_lead'
+            }
+        }
+    )
 })
--- kubejs/server_scripts/temp_removals.js
+++ kubejs/server_scripts/temp_removals.js
@@ -21,6 +21,9 @@ ServerEvents.recipes(event => {
 
     // Just Dire Things
     // event.remove({ mod: "justdirethings" })
+    event.remove({ output: "justdirethings:gooblock_tier2"})
+    event.remove({ output: "justdirethings:gooblock_tier3"})
+    event.remove({ output: "justdirethings:gooblock_tier4"})
 
     // Ender IO
     // event.remove({ mod: "enderio" })
