5ef5eb4c081cb7e4f2c1759f91e952924ef75ec8
PL45VYR <224156751+PL45VYR@users.noreply.github.com>
2026-05-31 10:05:45 -0400

    Updated a few mods
    Added Amalgametal
    
    Cursed Earth now needs Death Essence instead of Wither Roses

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -1,3 +1,9 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded startup example script)')
 
+StartupEvents.registry("item", event => {
+    event.create('kubejs:amalgametal')
+    .displayName('Amalga-metal')
+    .texture('kubejs:item/amalgametal')
+    .maxStackSize(64)
+})
