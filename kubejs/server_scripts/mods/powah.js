572f61fd18c647fefaf5295cb6e29a461426e5a8
PL45VYR <224156751+PL45VYR@users.noreply.github.com>
2026-06-26 22:53:52 -0400

    Changed recipes for all Powah crystals

--- kubejs/server_scripts/mods/powah.js
+++ kubejs/server_scripts/mods/powah.js
@@ -1,2 +1,70 @@
 ServerEvents.recipes(event => {
+    // Blazing Crystal Recipe
+    event.custom({
+        "type": "powah:energizing",
+        "energy": 120000,
+        "ingredients": [
+            {
+                "item": "malum:blazing_quartz"
+            }
+        ],
+        "result": {
+            "count": 1,
+            "id": "powah:crystal_blazing"
+        }
+    })
+
+    // Niotic Crystal Recipe
+    event.custom({
+        "type": "powah:energizing",
+        "energy": 300000,
+        "ingredients": [
+            {
+                "item": "actuallyadditions:diamatine_crystal"
+            }
+        ],
+        "result": {
+            "count": 1,
+            "id": "powah:crystal_niotic"
+        }
+    })
+
+    // Spirited Crystal Recipe
+    event.custom({
+        "type": "powah:energizing",
+        "energy": 1000000,
+        "ingredients": [
+            {
+                "item": "actuallyadditions:emeradic_crystal"
+            }
+        ],
+        "result": {
+            "count": 1,
+            "id": "powah:crystal_spirited"
+        }
+    })
+
+    // Nitro Crystal Recipe
+    event.custom({
+        "type": "powah:energizing",
+        "energy": 20000000,
+        "ingredients": [
+            {
+                "item": "extendedcrafting:flux_star"
+            },
+            {
+                "item": "actuallyadditions:restonia_crystal_block"
+            },
+            {
+                "item": "actuallyadditions:restonia_crystal_block"
+            },
+            {
+                "item": "powah:blazing_crystal_block"
+            }
+        ],
+        "result": {
+            "count": 4,
+            "id": "powah:crystal_nitro"
+        }
+    })
 })
