6cc55163ba51fa1c2c40f30437ed307837920114
PL45VYR <224156751+PL45VYR@users.noreply.github.com>
2026-06-30 14:51:08 -0400

    Changed Gray Paste recipe, added Blazebloom Goo recipe

--- kubejs/server_scripts/mods/occultism.js
+++ kubejs/server_scripts/mods/occultism.js
@@ -5,11 +5,11 @@ ServerEvents.recipes(event => {
     event.recipes.occultism.spirit_fire('malum:azure_runewood_sapling', 'malum:runewood_sapling')
 
     // Otherrock and Otherstone Recipe Tweaks
-    event.replaceInput({id: 'occultism:spirit_fire/otherrock'}, 'minecraft:diorite', 'malum:tainted_rock')
-    event.replaceInput({id: 'occultism:spirit_fire/otherstone'}, 'minecraft:andesite', 'malum:twisted_rock')
+    event.replaceInput({ id: 'occultism:spirit_fire/otherrock' }, 'minecraft:diorite', 'malum:tainted_rock')
+    event.replaceInput({ id: 'occultism:spirit_fire/otherstone' }, 'minecraft:andesite', 'malum:twisted_rock')
 
     // Research Fragment Recipe Tweaks
-    event.replaceInput({output: 'occultism:research_fragment_dust'}, 'minecraft:experience_bottle', 'forbidden_arcanus:xpetrified_orb')
+    event.replaceInput({ output: 'occultism:research_fragment_dust' }, 'minecraft:experience_bottle', 'forbidden_arcanus:xpetrified_orb')
 
     // First Frontier Recipe
     event.recipes.occultism.ritual(
@@ -28,4 +28,24 @@ ServerEvents.recipes(event => {
         'occultism:craft_djinni'
     ).dummy('kubejs:dummy_craft_frontier')
     .entityToSacrifice(EntityToSacrifice.of('friendsandfoes:glare', 'Glare'))
+
+    // Gray Paste Recipe Tweak
+    event.replaceInput({ output: 'occultism:gray_paste' }, 'minecraft:phantom_membrane', 'actuallyadditions:empowered_enori_crystal')
+
+    // Blazebloom Goo Recipe
+    event.recipes.occultism.ritual(
+        Item.of('justdirethings:gooblock_tier2'),
+        [
+            "justdirethings:gooblock_tier1",
+            "reliquary:molten_core",
+            "malum:blazing_quartz",
+            "minecraft:nether_wart",
+            "reliquary:molten_core",
+            "reliquary:molten_core",
+            "malum:blazing_quartz",
+            "minecraft:nether_wart",
+        ],
+        'occultism:book_of_binding_bound_afrit',
+        'occultism:craft_afrit'
+    ).dummy('kubejs:dummy_craft_blazebloom_goo')
 })
--- kubejs/startup_scripts/mods/occultism.js
+++ kubejs/startup_scripts/mods/occultism.js
@@ -1,6 +1,11 @@
 StartupEvents.registry('item', (event) => {
     event.create('dummy_craft_frontier', 'occultism:ritual_dummy')
-        .pentacleType("craft") //Determines the dummy texture, valid options are Valid options are: "misc", "craft", "summon", "possess", default is "misc".
+        .pentacleType("craft")
         .displayName('Ritual: Craft Frontier Gateway')
         .ritualTooltip('Cross the First Frontier by completing this gateway.')
+
+    event.create('dummy_craft_blazebloom_goo', 'occultism:ritual_dummy')
+        .pentacleType("craft") //Valid options are: "misc", "craft", "summon", "possess"
+        .displayName('Ritual: Craft Blazebloom Goo')
+        .ritualTooltip('Blazebloom Goo allows you to start creating Blazegold Ore.')
 })
